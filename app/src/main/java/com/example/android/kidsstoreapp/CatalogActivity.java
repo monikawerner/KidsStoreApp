1e8243210a9a1e6b467192b29cff4e21c3643743
Monika <monika.agnieszka.werner@gmail.com>
2018-06-21 12:49:27 +0200

    add activity_editor xml file

--- app/src/main/java/com/example/android/kidsstoreapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/kidsstoreapp/CatalogActivity.java
@@ -85,6 +85,38 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         Uri newUri = getContentResolver().insert(KidsEntry.CONTENT_URI, values);
     }
 
+            /**
+             * Helper method to delete all products in the database.
+             */
+            private void deleteAllProducts() {
+                int rowsDeleted = getContentResolver().delete(KidsEntry.CONTENT_URI, null, null);
+                Log.v("CatalogActivity", rowsDeleted + " rows deleted from products database");
+            }
+
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu options from the res/menu/menu_catalog.xml file.
+                // This adds menu items to the app bar.
+                getMenuInflater().inflate(R.menu.menu_catalog, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // User clicked on a menu option in the app bar overflow menu
+                switch (item.getItemId()) {
+                    // Respond to a click on the "Insert dummy data" menu option
+                    case R.id.action_insert_dummy_data:
+                        insertProduct();
+                        return true;
+                    // Respond to a click on the "Delete all entries" menu option
+                    case R.id.action_delete_all:
+                        deleteAllProducts();
+                        return true;
+                }
+                return super.onOptionsItemSelected(item);
+            }
+
             @Override
             public Loader<Cursor> onCreateLoader(int i, Bundle args) {
                 String[] projection = {
--- app/src/main/java/com/example/android/kidsstoreapp/data/KidsContract.java
+++ app/src/main/java/com/example/android/kidsstoreapp/data/KidsContract.java
@@ -12,7 +12,7 @@ import android.provider.BaseColumns;
 public final class KidsContract {
     private KidsContract() {}
 
-    public static final String CONTENT_AUTHORITY = "com.example.android.kids";
+    public static final String CONTENT_AUTHORITY = "com.example.android.kidsstoreapp";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_KIDS = "kids";
 
